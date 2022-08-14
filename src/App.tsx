48779eb332bedf044c95d137e94b5f0b92a1b204
Jason Savelli <jason.savelli@mention.com>
2022-08-14 19:45:43 +0200

    task: install react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
-import { useState, Suspense } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { graphql } from "relay-runtime";
 import { RelayEnvironmentProvider, loadQuery, usePreloadedQuery } from "react-relay/hooks";
 import relayEnvironment from "./relay-environment";
 import { Countries } from "./countries";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <RelayEnvironmentProvider environment={relayEnvironment}>
-      <Suspense fallback="Loading...">
-        <Countries />
-        <div className="App">
-          <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img src="/vite.svg" className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://reactjs.org" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </div>
-      </Suspense>
+      <Countries />
     </RelayEnvironmentProvider>
   );
-}
+};
 
 export default App;
